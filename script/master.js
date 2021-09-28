dadb05934c98a8189e960329dc3b75b659d51750
Ayush Priyadarshi <31592771+ayushp2002@users.noreply.github.com>
2021-09-28 21:55:38 +0530

    Dynamic date and time added to taskbar tray

--- script/master.js
+++ script/master.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+    var dt = new Date();
+    if (dt.getHours() >= 12) {
+        var time = (dt.getHours() - 12) + ":" + dt.getMinutes() + " PM";
+    } else {
+        var time = dt.getHours() + ":" + dt.getMinutes() + " AM";
+    }
+    var date = dt.getDate() + "/" + dt.getMonth() + "/" + dt.getFullYear()
+    $(".time").text(time);
+    $(".date").text(date);
     $(".start-button").on("click", function () {
         if ($(".start-menu").css("bottom") == "-700px") {
             $(".start-menu").css("bottom", ($(".taskbar").height() + 12) + "px");
