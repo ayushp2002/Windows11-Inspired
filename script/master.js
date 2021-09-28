0370329286ccb38ba3057e999bfc9ed6716b54e9
Ayush Priyadarshi <31592771+ayushp2002@users.noreply.github.com>
2021-09-28 20:44:54 +0530

    Animated working start menu added

---
+++ script/master.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".start-button").on("click", function () {
+        if ($(".start-menu").css("bottom") == "-600px") {
+            $(".start-menu").css("bottom", ($(".taskbar").height() + 12) + "px");
+        } else {
+            console.log($(".start-menu").css("bottom"));
+            $(".start-menu").css("bottom", "-600px");
+        }
+    });
+});
